e64da8cd42e339f0d9a56e49c1e939e6d0ad679a
iarrar <iarrar@users.noreply.github.com>
2024-09-10 23:01:55 +0200

    2FA implmented

--- backend/core-serializers.py
+++ backend/core-serializers.py
@@ -6,18 +6,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password', 'games_played', 'wins', 'losses')
+        fields = ('username', 'email', 'password', 'games_played', 'wins', 'losses', 'enable_2fa')
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            enable_2fa=validated_data.get('enable_2fa', False)
         )
         return user
     
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
+        instance.enable_2fa = validated_data.get('enable_2fa', instance.enable_2fa)
         instance.save()
         return instance
